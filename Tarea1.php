c947c5496920c875cbe61bb1eea01b9d12fbd11e
Pedro Ramos <ipedroanto@gmail.com>
2023-11-29 00:22:45 -0300

    Creación de cinco objetos

---
+++ Tarea1.php
@@ -0,0 +1,128 @@
+<?php
+class Superheroes {
+
+public $Personal_name;
+public $Code_name;
+public $Planet_of_origin;
+public $Powers;
+public $age;
+public $streng;
+
+}
+
+//un objeto de la clase Persona
+echo "<h2>objeto No 1</h2>";
+$Rokk_Krinn = new Superheroes();
+$Rokk_Krinn->Personal_name ="Rokk Krinn";
+$Rokk_Krinn->Code_name="Cosmic Boy";
+$Rokk_Krinn->Planet_of_origin="Braal";
+$Rokk_Krinn->Powers="Magnetism";
+$Rokk_Krinn->age=25;
+$Rokk_Krinn->streng=12;
+echo '<br>';
+echo $Rokk_Krinn->Personal_name;
+echo '<br>';
+echo $Rokk_Krinn->Code_name;
+echo '<br>';
+echo $Rokk_Krinn->Planet_of_origin;
+echo '<br>';
+echo $Rokk_Krinn->Powers;
+echo '<br>';
+echo $Rokk_Krinn->age;
+echo '<br>';
+echo $Rokk_Krinn->streng;
+echo '<br>';
+echo "<h2>objeto No 2</h2>";
+$Garth_Ranzz  = new Superheroes();
+$Garth_Ranzz->Personal_name ="Garth Ranzz";
+$Garth_Ranzz->Code_name="Lightning Lad ";
+$Garth_Ranzz->Planet_of_origin="Winath ";
+$Garth_Ranzz->Powers="Electrical manipulation";
+$Garth_Ranzz->age=35;
+$Garth_Ranzz->streng=10;
+echo '<br>';
+echo $Garth_Ranzz->Personal_name;
+echo '<br>';
+echo $Garth_Ranzz->Code_name;
+echo '<br>';
+echo $Garth_Ranzz->Planet_of_origin;
+echo '<br>';
+echo $Garth_Ranzz->Powers;
+echo '<br>';
+echo $Garth_Ranzz->age;
+echo '<br>';
+echo $Garth_Ranzz->streng;
+echo '<br>';
+echo "<h2>objeto No 3</h2>";
+$Reep_Daggle  = new Superheroes();
+$Reep_Daggle->Personal_name ="Reep Daggle";
+$Reep_Daggle->Code_name="Chameleon Boy";
+$Reep_Daggle->Planet_of_origin="Durla";
+$Reep_Daggle->Powers="Shapeshifting";
+$Reep_Daggle->age=31;
+$Reep_Daggle->streng=15;
+echo '<br>';
+echo $Reep_Daggle->Personal_name;
+echo '<br>';
+echo $Reep_Daggle->Code_name;
+echo '<br>';
+echo $Reep_Daggle->Planet_of_origin;
+echo '<br>';
+echo $Reep_Daggle->Powers;
+echo '<br>';
+echo $Reep_Daggle->age;
+echo '<br>';
+echo $Reep_Daggle->streng;
+echo '<br>';
+echo "<h2>objeto No 4</h2>";
+$Jo_Nah = new Superheroes();
+$Jo_Nah->Personal_name ="Jo Nah";
+$Jo_Nah->Code_name="Ultra Boy ";
+$Jo_Nah->Planet_of_origin="Rimbor";
+$Jo_Nah->Powers="Super-speed, super-strength, invulnerability";
+$Jo_Nah->age=21;
+$Jo_Nah->streng=10;
+echo '<br>';
+echo $Jo_Nah->Personal_name;
+echo '<br>';
+echo $Jo_Nah->Code_name;
+echo '<br>';
+echo $Jo_Nah->Planet_of_origin;
+echo '<br>';
+echo $Jo_Nah->Powers;
+echo '<br>';
+echo $Jo_Nah->age;
+echo '<br>';
+echo $Jo_Nah->streng;
+echo '<br>';
+echo "<h2>objeto No 5</h2>";
+$Jan_Arrah = new Superheroes();
+$Jan_Arrah->Personal_name ="Jan Arrah";
+$Jan_Arrah->Code_name="Mon-El";
+$Jan_Arrah->Planet_of_origin="Daxam";
+$Jan_Arrah->Powers="Mastery of martial arts";
+$Jan_Arrah->age=27;
+$Jan_Arrah->streng=16;
+echo '<br>';
+echo $Jan_Arrah->Personal_name;
+echo '<br>';
+echo $Jan_Arrah->Code_name;
+echo '<br>';
+echo $Jan_Arrah->Planet_of_origin;
+echo '<br>';
+echo $Jan_Arrah->Powers;
+echo '<br>';
+echo $Jan_Arrah->age;
+echo '<br>';
+echo $Jan_Arrah->streng;
+echo '<br>';
+
+
+
+
+
+
+?>
+
+
+
